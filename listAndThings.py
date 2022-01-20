cbc2a21857e20e15b6f8cfd141b587a7b6fa8faa
aariandhanani <aariandhanani@gmail.com>
2022-01-20 09:57:20 -0600

    Create listAndThings.py

---
+++ listAndThings.py
@@ -0,0 +1,14 @@
+#Aarian Dhanani
+#1/20/22
+#List, tuple, dictionary, set
+
+import os
+os.system('clear')
+
+fruits = ["orange", "apple", 1, 1.05, "banana"]
+print(len(fruits))
+for elem in fruits:
+    print(type(elem))
+
+myList = list((3,5,6))
+print(myList)
